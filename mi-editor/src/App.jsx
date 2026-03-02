11c900b41f70b6775e1c3656d900392a12b9fa88
rociobll <rocio.ballo27@gmail.com>
2026-03-02 17:25:16 +0100

    Summernote - useState para language y templates - sin acabar

--- mi-editor/src/App.jsx
+++ mi-editor/src/App.jsx
@@ -1,25 +1,47 @@
 import { useState } from "react";
 import Editor from "./components/Editor";
 
+// 👇 Datos iniciales (simulan base de datos)
+const initialTemplates = [
+  {
+    id: 1,
+    name: "Bienvenida ES",
+    language: "es-ES",
+    content: "<p>Hola {name}, bienvenido</p>"
+  },
+  {
+    id: 2,
+    name: "Welcome EN",
+    language: "en-US",
+    content: "<p>Hello {name}, welcome</p>"
+  }
+];
+
 
 function App() {
-
+  const [lang, setLanguage] = useState('es-ES')
+  const [templates, setTemplates] = useState(initialTemplates)
   const [content, setContent] = useState('')
 
   // guardar contenido - veo por consola
   const saveContent = () => {
+    const newTemplate= {
+      id: templates.length + 1,
+      name: "nueva plantilla"
+    }
     console.log('Contenido guardado.')
     console.log(content)
+   
   }
 
 
   return (
-    <div className="container editor-container">
+    <><div className="container editor-container">
       <div className="row justify-content-center">
         <h1>SUMMERNOTE - Prueba</h1>
-        <div className= "editor">
-        {/* le pasamos al componente la funcion setCointent para capturar cambios */}
-          <Editor onChange={setContent}/> 
+        <div className="editor">
+          {/* le pasamos al componente la funcion setCointent para capturar cambios */}
+          <Editor value={content} onChange={setContent} lang={lang} />
 
           boton para guardar contenido
           <button className="btn btn-success mt-4" onClick={saveContent}>
@@ -27,7 +49,35 @@ function App() {
           </button>
         </div>
       </div>
-    </div>
+    </div><div>
+        <select
+          className="form-select mt-3"
+          onChange={(e) => {
+            const selected = templates.find(t => t.id === Number(e.target.value));
+            if (selected) {
+              setContent(selected.content);
+            }
+          } }
+        >
+          <option value="">Selecciona una plantilla</option>
+          {templates.map(template => (
+            <option key={template.id} value={template.id}>
+              {template.name}
+            </option>
+          ))}
+        </select>
+        <select
+          className="form-select"
+          value={lang}
+          onChange={(e) => setLanguage(e.target.value)}
+        >
+          <option value="es-ES">Español</option>
+          <option value="en-US">English</option>
+          <option value="fr-FR">Francés</option>
+          <option value="it-IT">Italiano</option>
+
+        </select>
+      </div></>
   );
 }
 
--- mi-editor/src/components/Editor.jsx
+++ mi-editor/src/components/Editor.jsx
@@ -1,62 +1,69 @@
 import { useEffect, useRef } from "react";
 import "./Editor.css";
 
-function Editor({ onChange }) {
+
+// value para recibir contenido desde App y onchange para mandar contenido a App, y language para soporte multi
+function Editor({ value, onChange, lang }) {
+  // useRef para referenciar div del DOm deonde se montará summernote
   const editorRef = useRef(null);
 
   // tomar jquery del CDN (window.$)
   const $ = window.$;
 
   useEffect(() => {
+    
     // Inicializar Summernote
     $(editorRef.current).summernote({
       height: 300,
       placeholder: "Escriba aquí",
-      lang: "es-ES",
+      lang: lang,
       callbacks: {
         onFocus: function () {
-          const content = $(editorRef.current).summernote('code')
+          const content = $(editorRef.current).summernote("code")
 
           if (content === "<p><br></p>") {
-            $(editorRef.current).summernote('code', "")
+            $(editorRef.current).summernote("code", "")
           }
         },
         onChange: function (contents) {
-          onChange(contents);
+          // cada vez que el contenido cambia, llamamos a la función que nos pasa App.js
+          onChange(contents)
         },
       },
     });
 
+      if (value) {
+        $(editorRef.current).summernote("code", value);
+  }
+
     // Limpiar al desmontar
     return () => {
-      $(editorRef.current).summernote("destroy");
+      $(editorRef.current).summernote("destroy")
     };
-  }, []);
+  }, [$, lang]);  //cuando cambie idioma se destruye y vuelve a crear otro editor con nuevo idioma
 
-  //  cargar contenido en el editor
-  const loadContent = () => {
-    const template = "<p>Hola {name}, bienvenido a tu nota de hoy</p>";
-    // code de summernote pone el contenido html dentro de editor
-    $(editorRef.current).summernote("code", template);
-    // actualizar estado de React para q App tenga el contenido
-    onChange(template);
-  };
+  useEffect(() => {
+   if (value !== undefined && editorRef.current) {
+    $(editorRef.current).summernote("code", value);
+   }
+  }, [$, value]);
 
-  // funcion insertar placeholders en pos cursor
+  // //  cargar contenido en el editor
+  // const loadContent = () => {
+  //   const template = "<p>Hola {name}, bienvenido a tu nota de hoy</p>";
+  //   // code de summernote pone el contenido html dentro de editor
+  //   $(editorRef.current).summernote("code", template);
+  //   // actualizar estado de React para q App tenga el contenido
+  //   onChange(template);
+  // };
+
+  // funcion insertar placeholders en posicion cursor
   const insertPlaceHolder = (placeholder) => {
-    $(editorRef.current).summernote("insertText", `{${placeholder}`);
+    $(editorRef.current).summernote("insertText", `{${placeholder}}`);
   };
 
   return (
-    // <div className="formulario">
-    // <form action="">
-    //   <label htmlFor="name">Nombre</label>
-    //   <input type="text" id="name" name="user_name" />
-
-    //     <label htmlFor="date">Fecha</label>
-    //   <input type="datetime-local" id="date" name="date" />
-      
-    // </form>
+ 
     <div>
       {/* aqui se monta summernote */}
       <div ref={editorRef}></div>
@@ -79,7 +86,9 @@ function Editor({ onChange }) {
         </button>
 
         {/* boton cargar contenido */}
-        <button className="btn btn-warning" onClick={loadContent}>
+        {/* <button className="btn btn-warning" onClick={loadContent}> */}
+        <button className="btn btn-warning">
+
           Cargar contenido
         </button>
       </div>
