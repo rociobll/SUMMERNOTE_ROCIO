41fb30c38d3329f1275ee83c547029624d33526e
rociobll <rocio.ballo27@gmail.com>
2026-03-02 16:03:31 +0100

    Summernote - useState(content, setContent) y funcion saveContent,insertPlaceholder

--- mi-editor/src/App.jsx
+++ mi-editor/src/App.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import Editor from "./components/Editor";
 
 
 function App() {
+
+  const [content, setContent] = useState('')
+
+  // guardar contenido - veo por consola
+  const saveContent = () => {
+    console.log('Contenido guardado.')
+    console.log(content)
+  }
+
+
   return (
     <div className="container editor-container">
       <div className="row justify-content-center">
         <h1>SUMMERNOTE - Prueba</h1>
         <div className= "editor">
-          <Editor />
+        {/* le pasamos al componente la funcion setCointent para capturar cambios */}
+          <Editor onChange={setContent}/> 
+
+          boton para guardar contenido
+          <button className="btn btn-success mt-4" onClick={saveContent}>
+            Guardar contenido
+          </button>
         </div>
       </div>
     </div>
--- mi-editor/src/components/Editor.jsx
+++ mi-editor/src/components/Editor.jsx
@@ -1,29 +1,91 @@
-import { useEffect, useRef } from "react"
-import './Editor.css'
+import { useEffect, useRef } from "react";
+import "./Editor.css";
 
-function Editor() {
-  const editorRef = useRef(null)
+function Editor({ onChange }) {
+  const editorRef = useRef(null);
 
   // tomar jquery del CDN (window.$)
-  const $ = window.$
+  const $ = window.$;
 
   useEffect(() => {
     // Inicializar Summernote
     $(editorRef.current).summernote({
       height: 300,
-      placeholder: "Escribe aquí...",
-      lang: 'es-ES'
-    });
+      placeholder: "Escriba aquí",
+      lang: "es-ES",
+      callbacks: {
+        onFocus: function () {
+          const content = $(editorRef.current).summernote('code')
 
+          if (content === "<p><br></p>") {
+            $(editorRef.current).summernote('code', "")
+          }
+        },
+        onChange: function (contents) {
+          onChange(contents);
+        },
+      },
+    });
 
     // Limpiar al desmontar
     return () => {
-      $(editorRef.current).summernote("destroy")
+      $(editorRef.current).summernote("destroy");
     };
   }, []);
 
-  
-  return <div ref={editorRef}></div>;
+  //  cargar contenido en el editor
+  const loadContent = () => {
+    const template = "<p>Hola {name}, bienvenido a tu nota de hoy</p>";
+    // code de summernote pone el contenido html dentro de editor
+    $(editorRef.current).summernote("code", template);
+    // actualizar estado de React para q App tenga el contenido
+    onChange(template);
+  };
+
+  // funcion insertar placeholders en pos cursor
+  const insertPlaceHolder = (placeholder) => {
+    $(editorRef.current).summernote("insertText", `{${placeholder}`);
+  };
+
+  return (
+    // <div className="formulario">
+    // <form action="">
+    //   <label htmlFor="name">Nombre</label>
+    //   <input type="text" id="name" name="user_name" />
+
+    //     <label htmlFor="date">Fecha</label>
+    //   <input type="datetime-local" id="date" name="date" />
+      
+    // </form>
+    <div>
+      {/* aqui se monta summernote */}
+      <div ref={editorRef}></div>
+
+      {/* botones para placeholders */}
+      <div className="mt-3">
+        <p>Insertar variable:</p>
+        <button
+          className="btn -btn-info me-2"
+          onClick={() => insertPlaceHolder("name")}
+        >
+          {"{name}"}
+        </button>
+
+        <button
+          className="btn -btn-info"
+          onClick={() => insertPlaceHolder("date")}
+        >
+          {"{date}"}
+        </button>
+
+        {/* boton cargar contenido */}
+        <button className="btn btn-warning" onClick={loadContent}>
+          Cargar contenido
+        </button>
+      </div>
+    </div>
+    // </div>
+  );
 }
 
 export default Editor;
